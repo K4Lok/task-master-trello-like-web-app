9ca8f611641f33678a6dd34bf905f5b2945b8a27
K4Lok <samkalok107@gmail.com>
2023-04-23 17:37:27 +0800

    update: email value from formdata and POST fetch

--- frontend/scripts/form.js
+++ frontend/scripts/form.js
@@ -5,10 +5,24 @@ const actionBtn = document.querySelector('.action-btn');
 
 signupForm.addEventListener('submit', handleSignupForm);
 
-function handleSignupForm(e) {
+async function handleSignupForm(e) {
     e.preventDefault();
-    console.log(e);
+
     actionBtn.innerHTML = '<div class="loader"></div>';
     actionBtn.parentElement.style.cursor = 'not-allowed';
     actionBtn.style.pointerEvents = 'none';
+
+    const form = new FormData(signupForm);
+    const username = form.get('username');
+    const password = form.get('password');
+    const email = form.get('email');
+
+    const res = await fetch('http://localhost:5050/signup', {
+        method: "POST",
+        mode: "no-cors",
+        body: JSON.stringify({username, password, email})
+});
+    const json = await res.json();
+
+    console.log(json);
 }
