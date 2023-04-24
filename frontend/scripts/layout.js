f7029d2f2098296204ba51a7d67156e567750092
ImK4Lok <samkalok107@gmail.com>
2023-04-25 00:47:10 +0800

    add: logout logic and redirect to login page

--- frontend/scripts/layout.js
+++ frontend/scripts/layout.js
@@ -1,12 +1,35 @@
 const toggleBtn = document.getElementById('toggle-btn');
 const sidebar = document.querySelector('aside');
 
-toggleBtn.addEventListener('click', handleToggleSidebar);
+if (toggleBtn) toggleBtn.addEventListener('click', handleToggleSidebar);
 
 let isShowSidebar = true;
 // sidebar.style.display = 'none';
 
+// Controll Sidebar toggle event
 function handleToggleSidebar() {
     isShowSidebar = !isShowSidebar;
     sidebar.style.display = isShowSidebar == true ? 'block' : 'none';
+}
+
+const userBtnGroup = document.getElementById('user-btn-group');
+
+// Display header right corner button based on session
+function controlUserButtonGroup() {
+    const sessionId = Cookies.get('PHPSESSID');
+    const uemail = Cookies.get('uemail');
+
+    if (!sessionId || !uemail) {
+        return;
+    }
+
+    userBtnGroup.innerHTML = `<button onClick="handleLogout()"><li>Log out</li></button>`;
+}
+
+controlUserButtonGroup();
+
+function handleLogout() {
+    Cookies.remove('uemail', { path: '/', domain: '127.0.0.1'});
+    Cookies.remove('PHPSESSID', { path: '/', domain: '127.0.0.1'});
+    location.reload();
 }
