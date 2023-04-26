6aee0fdefd7355fee678a9bae17695b3e80a3ddc
ImK4Lok <samkalok107@gmail.com>
2023-04-26 23:39:06 +0800

    comment: categorize routes

--- backend/routes.php
+++ backend/routes.php
@@ -22,30 +22,23 @@ $route->post('/signup', fn() => UserController::signup());
 $route->post('/login', fn() => UserController::login());
 $route->post('/auth', fn() => UserController::auth());
 
+// Task Board Routes
 $route->get('/api/task-board', fn() => TaskBoardController::all());
-
 $route->post('/api/task-board/create', fn() => TaskBoardController::create());
-
 $route->post('/api/task-board/update', fn() => TaskBoardController::update());
-
 $route->post('/api/task-board/delete', fn() => TaskBoardController::delete());
-
 $route->get('/api/task-board/id', fn() => TaskBoardController::name());
 
+// Task Section Routes
 $route->get('/api/task-section', fn() => TaskSectionController::all());
-
 $route->post('/api/task-section/create', fn() => TaskSectionController::create());
-
 $route->post('/api/task-section/update', fn() => TaskSectionController::update());
-
 $route->post('/api/task-section/delete', fn() => TaskSectionController::delete());
 
+// Task Routes
 $route->get('/api/task', fn() => TaskController::all());
-
 $route->post('/api/task/create', fn() => TaskController::create());
-
 $route->post('/api/task/move', fn() => TaskController::move());
-
 $route->post('/api/task/sort', fn() => TaskController::sort());
 
 $route->run();
