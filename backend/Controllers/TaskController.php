d09042ad7a52c8e8c509815891dcd5d501ddf8b6
ImK4Lok <samkalok107@gmail.com>
2023-04-26 23:26:28 +0800

    refactor: TaskController create

--- backend/Controllers/TaskController.php
+++ backend/Controllers/TaskController.php
@@ -18,4 +18,41 @@ Class TaskController {
         echo json_encode(["data" => $tasks, "succeed" => true]);
         exit();
     }
+
+    public static function create() {
+        header('Content-Type: application/json; charset=utf-8');
+
+        global $auth;
+        $auth->post_auth();
+
+        if (!isset($_POST['task-board-id']) || !isset($_POST['task-section-id']) || !isset($_POST['task-name']) || !isset($_POST['description']) || !isset($_POST['complete-date'])) {
+            echo json_encode(["message" => "Data is missing!", "succeed" => false]);
+            exit();
+        }
+
+        $taskBoardId = $_POST['task-board-id'];
+        $taskSectionId = $_POST['task-section-id'];
+        $name = $_POST['task-name'];
+        $description = $_POST['description'];
+        $completeDate = $_POST['complete-date'];
+
+        $data = [
+            'task-board-id' => $taskBoardId,
+            'task-section-id' => $taskSectionId,
+            "name" => $name, 
+            "description" => $description,
+            "complete-date" => $completeDate,
+        ];
+
+        $model = new DataModel();
+        $isSucceed = $model->createTask($taskBoardId, $data);
+
+        if (!$isSucceed) {
+            echo json_encode(["message" => "We faced some issues on creating task board, please try again!", "succeed" => false]);
+            exit();
+        }
+
+        echo json_encode(["message" => "Task Board created!", "succeed" => true]);
+        exit();
+    }
 }
--- backend/routes.php
+++ backend/routes.php
@@ -84,45 +84,12 @@ $route->post('/api/task-section/delete', function() {
     TaskSectionController::delete();
 });
 
-$route->post('/api/task/create', function() {
-    header('Content-Type: application/json; charset=utf-8');
-
-    global $auth;
-    $auth->post_auth();
-
-    if (!isset($_POST['task-board-id']) || !isset($_POST['task-section-id']) || !isset($_POST['task-name']) || !isset($_POST['description']) || !isset($_POST['complete-date'])) {
-        echo json_encode(["message" => "Data is missing!", "succeed" => false]);
-        exit();
-    }
-
-    $taskBoardId = $_POST['task-board-id'];
-    $taskSectionId = $_POST['task-section-id'];
-    $name = $_POST['task-name'];
-    $description = $_POST['description'];
-    $completeDate = $_POST['complete-date'];
-
-    $data = [
-        'task-board-id' => $taskBoardId,
-        'task-section-id' => $taskSectionId,
-        "name" => $name, 
-        "description" => $description,
-        "complete-date" => $completeDate,
-    ];
-
-    $model = new DataModel();
-    $isSucceed = $model->createTask($taskBoardId, $data);
-
-    if (!$isSucceed) {
-        echo json_encode(["message" => "We faced some issues on creating task board, please try again!", "succeed" => false]);
-        exit();
-    }
-
-    echo json_encode(["message" => "Task Board created!", "succeed" => true]);
-    exit();
-});
-
 $route->get('/api/task', function() {
     TaskController::all();
+});
+
+$route->post('/api/task/create', function() {
+    TaskController::create();
 });
 
 $route->post('/api/task/move', function() {
